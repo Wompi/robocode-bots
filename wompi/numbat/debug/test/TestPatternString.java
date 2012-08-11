642cb2ace461ad039206a3263634a685f1cbb8f9
Wompi <robocode@casepool.de>
2012-08-11 10:25:10 +0200

    TestPattern now hashed by target
    This can very nice show how the radar is working on melee fields, and also how the skipped turns inflict the pattern

--- wompi/numbat/debug/test/TestPatternString.java
+++ wompi/numbat/debug/test/TestPatternString.java
@@ -11,35 +11,76 @@
  ******************************************************************************/
 package wompi.numbat.debug.test;
 
+import java.util.HashMap;
+import java.util.Map.Entry;
+
 public class TestPatternString
 {
-	StringBuilder	myHistory;
-
-	long			lastStatusTime;
+	HashMap<Integer, StringBuilder>	allHistory;
+	HashMap<String, Integer>		botMap;
+	//StringBuilder					myHistory;
+	int								botIndex;
+	long							lastStatusTime;
 
 	public TestPatternString()
 	{
-		myHistory = new StringBuilder();
+		//		myHistory = new StringBuilder();
+		botMap = new HashMap<String, Integer>();
+		allHistory = new HashMap<Integer, StringBuilder>();
+		for (int i = 0; i < 9; i++)
+		{
+			allHistory.put(i, new StringBuilder());
+		}
 	}
 
 	public void registerHistoryPlaceHolder(long time)
 	{
 		long timeDiff = time - lastStatusTime;
 
-		if (timeDiff == 1) myHistory.insert(0, 'x');
-		else if (timeDiff > 1)
+		for (StringBuilder sb : allHistory.values())
 		{
-			for (long i = 0; i < (timeDiff - 1); i++)
+			if (timeDiff == 1) sb.insert(0, 'x');
+			else if (timeDiff > 1)
 			{
-				myHistory.insert(0, 'o');
+				for (long i = 0; i < (timeDiff - 1); i++)
+				{
+					sb.insert(0, 'o');
+				}
+				sb.insert(0, 'x');
 			}
-			myHistory.insert(0, 'x');
 		}
+		lastStatusTime = time;
+	}
+
+	public void registerScan(long time, String name)
+	{
+		Integer bot = botMap.get(name);
+		if (bot == null)
+		{
+			botMap.put(name, bot = botIndex++);
+		}
+		allHistory.get(bot).setCharAt(0, 'h');
 	}
 
 	public void printHistory(long time)
 	{
-		System.out.format("[%d] %s\n", time, myHistory.substring(0, (int) Math.min(70, time)));
+		for (Entry<Integer, StringBuilder> entry : allHistory.entrySet())
+		{
+			int bot = entry.getKey();
+			StringBuilder hist = entry.getValue();
+			String botName = "none";
+			for (Entry<String, Integer> db : botMap.entrySet())
+			{
+				String dbName = db.getKey();
+				int dbIndex = db.getValue();
+				if (dbIndex == bot)
+				{
+					botName = dbName;
+					break;
+				}
+			}
+			System.out.format("[%d] %s - %s\n", time, hist.substring(0, (int) Math.min(70, time)), botName);
+		}
+		System.out.format("\n");
 	}
-
 }
