4f4400abeb9acd8fa1ca5a48d16aca03c92b68d5
Wompi <robocode@casepool.de>
2012-08-13 23:20:56 +0200

    Just implemented the new method
    Singletick don't need this method it is mainly for multitick but for design reasons is it there.

--- wompi/numbat/gun/misc/NumbatSingleHolder.java
+++ wompi/numbat/gun/misc/NumbatSingleHolder.java
@@ -15,8 +15,13 @@ import java.util.HashSet;
 
 public class NumbatSingleHolder implements INumbatTick, Comparable<NumbatSingleHolder>
 {
+	final static int				DELTA_HEADING_INDEX	= 20;
+	final static int				VELOCITY_INDEX		= 8;
+	final static double				HEAD_FACTOR			= 2.0;
+	final static double				VELO_FACTOR			= 1.0;
+
 	public static int				classCount;
-	public static HashSet<Integer>	classIDStats	= new HashSet<Integer>();
+	public static HashSet<Integer>	classIDStats		= new HashSet<Integer>();
 	public static int				maxCount;
 	public static int				avgSum;
 	public static int				avg;
@@ -24,18 +29,26 @@ public class NumbatSingleHolder implements INumbatTick, Comparable<NumbatSingleH
 	public int						myID;
 	public int						vCount;
 
-	public NumbatSingleHolder(int id)
+	public double					tHeadingDelta;
+	public double					tVelocity;
+
+	public NumbatSingleHolder(double deltaHeading, double velocity)
 	{
-		myID = id;
+		tHeadingDelta = deltaHeading;
+		tVelocity = velocity;
+		myID = getEncodedID(deltaHeading, velocity);
 		classCount++;
 		classIDStats.add(myID);
 	}
 
+	@Override
 	public NumbatSingleHolder getMaxTick()
 	{
+		//System.out.format("single\n");
 		return this;
 	}
 
+	@Override
 	public boolean incrementCount(int id)
 	{
 		if (id == myID)
@@ -50,13 +63,26 @@ public class NumbatSingleHolder implements INumbatTick, Comparable<NumbatSingleH
 	}
 
 	@Override
+	public void addTick(double deltaHeading, double velocity)
+	{
+		throw new UnsupportedOperationException("NumbatSingleHolder can not add Ticks!");
+	}
+
+	@Override
 	public int compareTo(NumbatSingleHolder o)
 	{
 		return o.vCount - this.vCount; // descending order
 	}
 
-	public static NumbatSingleHolder getNewInstance(int id)
+	public static int getEncodedID(double deltaHead, double velocity)
 	{
-		return new NumbatSingleHolder(id);
+		int headInt = (int) Math.rint(Math.toDegrees(deltaHead * HEAD_FACTOR)) + DELTA_HEADING_INDEX;
+		int veoInt = (int) (Math.rint(velocity * VELO_FACTOR)) + VELOCITY_INDEX;
+		return (((headInt) << 8) + (veoInt));
+	}
+
+	public static NumbatSingleHolder getNewInstance(double deltaHead, double velocity)
+	{
+		return new NumbatSingleHolder(deltaHead, velocity);
 	}
 }
