bbc3b482d2a0af626d56124d56d25b2041a8aa5b
Wompi <robocode@casepool.de>
2013-03-03 21:24:54 +0100

    Bilby - just a better chase gun

--- wompi/Bilby.java
+++ wompi/Bilby.java
@@ -16,14 +16,24 @@ import robocode.BulletHitEvent;
 import robocode.Rules;
 import robocode.ScannedRobotEvent;
 import robocode.StatusEvent;
+import robocode.util.Utils;
 
 public class Bilby extends AdvancedRobot
 {
 	double	bTurns;
 	double	MAX_BULLET_POWER	= 3.0;
 
+	double	lastBulletPower;
+	double	bPower;
+	double	lastFireDistance;
+	String	lastName;
+
+	@Override
 	public void run()
 	{
+		setAdjustGunForRobotTurn(true);
+		setAdjustRadarForRobotTurn(true);
+		setAdjustRadarForGunTurn(true);
 		setTurnRadarRightRadians(Double.POSITIVE_INFINITY);
 		System.out.format("[%d] INIT\n", getTime());
 	}
@@ -31,40 +41,43 @@ public class Bilby extends AdvancedRobot
 	@Override
 	public void onStatus(StatusEvent e)
 	{
-		System.out.format("[%d] STATUS\n", e.getTime());
+		//System.out.format("[%d] STATUS\n", e.getTime());
 	}
 
+	@Override
 	public void onScannedRobot(ScannedRobotEvent e)
 	{
 		double absoluteBearing = e.getBearingRadians() + getHeadingRadians();
-		double distance = e.getDistance();
-		setTurnGunRightRadians(robocode.util.Utils.normalRelativeAngle(absoluteBearing - getGunHeadingRadians())); // head-on gun
 
-		setTurnRadarRightRadians(-getRadarTurnRemainingRadians());   // / just a simple RadarLock
+		setTurnRadarLeftRadians(getRadarTurnRemainingRadians()); // / just a simple RadarLock
 
-		if (getGunTurnRemaining() == 0)  											// it's usefull to wait with shoting until the gun is in position
+		if (getTime() > 29 && getGunHeat() == 0)
 		{
-			distance -= 18;
-			double diff = bTurns - getTime();
-			double buffy = (20 - distance / (diff)) / 3;
-			double bPower = Math.min(MAX_BULLET_POWER, Math.max(buffy, 0.1));
-
-			if (bPower <= 0.1)
-			{
-				bTurns = 0;
-			}
-
-			System.out.format("bPower[%d] bPower=%3.2f bTurns=%3.2f diff=%3.2f buffy=%3.2f gunHeat=%3.2f\n", getTime(), bPower, bTurns, diff, buffy,
-					getGunHeat() / getGunCoolingRate());
-			if (setFireBullet(bPower) != null)
-			{
-				if (diff <= (0)) bTurns = getTime() + distance / Rules.getBulletSpeed(bPower);
-
-				System.out.format("SHOT[%d] %3.2f bTurns=%3.2f dist=%3.2f\n", getTime(), bPower, bTurns, distance);
-			}
+			setAhead(e.getDistance());
 		}
 
+		if (getGunTurnRemaining() == 0 && e.getName().equals(lastName)) // it's usefull to wait with shoting until the gun is in position
+		{
+			double lastSpeed = Rules.getBulletSpeed(lastBulletPower);
+			double lastHeatTurns = Rules.getGunHeat(lastBulletPower) / getGunCoolingRate();
+			bPower = Math.min(Rules.MAX_BULLET_POWER,
+					(20.0 - (e.getDistance() / ((lastFireDistance / lastSpeed) - lastHeatTurns))) / 3.0);
+
+			if (bPower < 0.1) bPower = Rules.MAX_BULLET_POWER;
+
+			if (setFireBullet(bPower) != null)
+			{
+				System.out.format("[%04d] bPower=%3.5f dist=%3.5f \n", getTime(), bPower, e.getDistance());
+				lastFireDistance = e.getDistance();
+				lastBulletPower = bPower;
+			}
+		}
+		setTurnGunRightRadians(Utils.normalRelativeAngle(absoluteBearing - getGunHeadingRadians())); // head-on gun
+
+		setTurnRightRadians(Math.sin(e.getBearingRadians()));
+
 		// Test this against multiple sample.SittingDuck - then remove it and look what has changed :)
+		lastName = e.getName();
 		clearAllEvents();
 	}
 
