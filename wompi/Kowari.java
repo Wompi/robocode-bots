f6c2e9d90a566e92789f9b5a910c156888ab2141
Wompi <robocode@casepool.de>
2013-04-13 08:29:33 +0200

    Kowari - some test around radar gun and bullet coding

--- wompi/Kowari.java
+++ wompi/Kowari.java
@@ -1,12 +1,14 @@
 package wompi;
 
-import java.math.BigInteger;
-import java.util.Arrays;
+import java.awt.geom.Point2D;
 
 import robocode.AdvancedRobot;
+import robocode.Bullet;
 import robocode.BulletHitEvent;
+import robocode.Condition;
 import robocode.HitByBulletEvent;
 import robocode.HitWallEvent;
+import robocode.Rules;
 import robocode.ScannedRobotEvent;
 import robocode.StatusEvent;
 
@@ -79,8 +81,8 @@ import robocode.StatusEvent;
 
 public class Kowari extends AdvancedRobot
 {
-	private static final double	ADVANCE_FACTOR	= 1.0 / 2000.0;		// 2500 = 16deg 2000 = 20deg
-	private static final double	DISTANCE_FACTOR	= 176;					// 3.0 and 16 tick cooldown = 11*16= 176 = only one bullet in the air
+	private static final double	ADVANCE_FACTOR	= 1.0 / 2000.0;												// 2500 = 16deg 2000 = 20deg
+	private static final double	DISTANCE_FACTOR	= 176;															// 3.0 and 16 tick cooldown = 11*16= 176 = only one bullet in the air
 	private static final double	HIT_FACTOR		= Math.PI * 14.0 / 4.0;
 
 	private static double		eEnergy;
@@ -89,30 +91,47 @@ public class Kowari extends AdvancedRobot
 	private static long			lastHit;
 	private static double		dFactor;
 
-	private static int[]		entry			= new int[31];
+	private static double		enemyX;
+	private static double		enemyY;
+
+	private static final String	bulletcode		= "" + (char) 30 + (char) 29 + (char) 28 + (char) 27 + (char) 26
+														+ (char) 25 + (char) 24 + (char) 23 + (char) 22 + (char) 21
+														+ (char) 20 + (char) 19 + (char) 18 + (char) 17 + (char) 16
+														+ (char) 15 + (char) 14 + (char) 13 + (char) 12 + (char) 11
+														+ (char) 10 + (char) 9 + (char) 8 + (char) 7 + (char) 6
+														+ (char) 5 + (char) 4 + (char) 3 + (char) 2 + (char) 1;
+
+	private static double		eVelo;
+	private static long			eScan;
 
 	public Kowari()
-	{
-		int[] tmp = entry.clone();
-		Arrays.sort(tmp);
-		int bestindex = BigInteger.valueOf(tmp[0]).getLowestSetBit();
-
-	}
+	{}
 
 	@Override
 	public void run()
 	{
-		setAdjustGunForRobotTurn(true);
-		setTurnGunRightRadians(dir = Double.POSITIVE_INFINITY);
+		//setAdjustGunForRobotTurn(true);
+		dir = Double.POSITIVE_INFINITY;
 	}
 
 	@Override
 	public void onStatus(StatusEvent e)
-	{}
+	{
+		setTurnGunRightRadians(Rules.GUN_TURN_RATE_RADIANS);
+	}
 
 	@Override
-	public void onScannedRobot(ScannedRobotEvent e)
+	public void onScannedRobot(final ScannedRobotEvent e)
 	{
+		// 197
+		int index = (int) (e.getDistance() / 29.0);
+		char value = bulletcode.charAt(index);
+		double d = (short) value / 10.0;
+
+		double absBear = e.getBearingRadians() + getHeadingRadians();
+		enemyX = getX() + Math.sin(absBear) * e.getDistance();
+		enemyY = getY() + Math.cos(absBear) * e.getDistance();
+
 		//@formatter:off
 		setTurnRightRadians(
 				((200 * Math.signum(e.getVelocity()))- e.getDistance())
@@ -127,12 +146,30 @@ public class Kowari extends AdvancedRobot
 		if (((eEnergy - (eEnergy = e.getEnergy()))) > 0)
 		{
 			if (Math.cos(dirChange) < 0) onHitWall(null); // saves 2 byte compared to dir = - dir
-
 		}
 
-		setFire(200 / e.getDistance());
+		Bullet b = setFireBullet(d);
+		if (b != null)
+		{
+			//myBullets.add(b);
+
+			//saddCustomEvent(new KowariWave(getX(), getY(), absBear, d, this));
+		}
+
 		setMaxVelocity(1800 / e.getDistance());
 		setAhead(dir);
+
+		//@formatter:off
+		System.out.format("[%04d] acc=%3.5f sdiff=%d %s \n", 
+				getTime(), 
+				e.getVelocity() - eVelo, 
+				getTime() - eScan,
+				b != null ? "FIRE" : ""
+		);
+		eVelo = e.getVelocity();
+		eScan = getTime();
+		//@formatter:on
+
 	}
 
 	@Override
@@ -153,4 +190,52 @@ public class Kowari extends AdvancedRobot
 		eEnergy = e.getEnergy();
 	}
 
+	static class KowariWave extends Condition
+	{
+		//Global variables.	
+		double			absoluteBearing;
+		double			gunX;
+		double			gunY;
+		double			power;
+
+		private double	distanceTraveled;
+
+		static double	bearingOffset;
+
+		AdvancedRobot	myBot;
+
+		//Initialize this instance of the Wave class.
+		public KowariWave(double _gunX, double _gunY, double _absoluteBearing, double bPower, AdvancedRobot bot)
+		{
+			gunX = _gunX;
+			gunY = _gunY;
+			absoluteBearing = _absoluteBearing;
+			power = bPower;
+			myBot = bot;
+		}
+
+		@Override
+		public boolean test()
+		{
+			//Local variables.
+			double predictedX;
+			double predictedY;
+
+			//When the wave hits, calculate the bearing offset.
+			if ((distanceTraveled += Rules.getBulletSpeed(power)) > Math.hypot(predictedX = (enemyX - gunX),
+					predictedY = (enemyY - gunY)))
+			{
+				bearingOffset = Math.atan2(predictedX, predictedY) - absoluteBearing;
+				double bCodeDist = (3 - power) * 10 * 29;
+				double bRealDist = Point2D.distance(myBot.getX(), myBot.getY(), enemyX, enemyY);
+				double delta = bRealDist - bCodeDist;
+
+				System.out.format("[%04d] bullet out of range! p=%3.4f d=%3.5f d0=%3.5f delta=%3.5f\n",
+						myBot.getTime(), power, bCodeDist, bRealDist, delta);
+				myBot.removeCustomEvent(this);
+
+			}
+			return false;
+		}
+	}
 }
