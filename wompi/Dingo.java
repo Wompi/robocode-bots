69db7ffa59d0a696a25cf6a2a32a10c0ffed99bc
Wompi <robocode@casepool.de>
2014-05-25 07:14:27 +0200

    New bot develop - some sort of influence map (later) - some sort movement visualization (now) - development of an general structure for bots

--- wompi/Dingo.java
+++ wompi/Dingo.java
@@ -4,280 +4,144 @@ import java.awt.Color;
 import java.awt.Graphics2D;
 
 import robocode.AdvancedRobot;
-import robocode.Bullet;
 import robocode.BulletHitEvent;
 import robocode.HitByBulletEvent;
 import robocode.HitRobotEvent;
 import robocode.HitWallEvent;
 import robocode.ScannedRobotEvent;
 import robocode.StatusEvent;
-import robocode.util.Utils;
-import wompi.dingo.DingoEnergyDrop;
-import wompi.dingo.DingoFireDetection;
-import wompi.dingo.DingoInactivity;
-import wompi.paint.PaintEnemyBulletWaves;
-import wompi.paint.PaintEscapePath;
-import wompi.paint.PaintMaxEscapeAngle;
-import wompi.paint.PaintWallHeadingDistance;
+import wompi.mics.TimeProfile;
 import wompi.radar.ARadar;
-import wompi.radar.RadarSingleLock;
+import wompi.radar.IRadar;
+import wompi.radar.RadarNone;
 import wompi.radar.RadarStartSearch;
-import wompi.robomath.WallHeadingDistance;
+import wompi.robotcontrol.IRobotTurn;
+import wompi.target.ATargetHandler;
+import wompi.target.ITargetHandler;
+import wompi.target.SimpleTargetHandler;
 
-public class Dingo extends AdvancedRobot
+public class Dingo extends AdvancedRobot implements IRadar, IRobotTurn, ITargetHandler
 {
-	public static final double				DIST			= 185;
-	public static final double				PI_45			= Math.PI / 4.0;
-	public static final double				PI_360			= Math.PI * 2.0;
-	public static final double				PI_90			= Math.PI / 2.0;
-	public static final double				PI				= Math.PI;
-	public final static double				BORDER			= 18.0;
-	private final static double				INF				= Double.POSITIVE_INFINITY;
+	private ARadar					myRadar;
+	private static ATargetHandler	myTargetHandler;
 
-	private final ARadar					myRadar;
-
-	private final DingoFireDetection		myFireDetector;
-	private final DingoInactivity			myInactivity;
-	private final DingoEnergyDrop			myEnergyDrop;
-	private final WallHeadingDistance		myWallDistance;
-
-	// debug visualization
-	private final PaintWallHeadingDistance	myPaintWallDistance;
-	private final PaintEnemyBulletWaves		myBulletPaint;
-	private final PaintMaxEscapeAngle		myPaintMaxEscape;
-	private final PaintEscapePath			myPaintEscapePath;
-
-	private long							lastScan;
-
-	private double							xg;
-	private double							yg;
-	private double							eAbsBearing;
-	private double							eBearing;
-	private double							eHeading;
-	private double							eEnergy;
-	private double							eDistance;
-
-	// test 
-	int										wallHitIndex	= -1;
-	boolean									iFire;
+	// TODO: this should be a list or map where every device can register its amount 
+	// then the robot can decide which one it will take
+	// Lets say the radar wants to turn the body but the body wants something other so decide wich one is
+	// the best choice for now
+	private double					myRadarTurnAmount;
+	private double					myGunTurnAmount;
+	private double					myBodyTurnAmount;
 
 	public Dingo()
 	{
-		myRadar = new RadarStartSearch(new RadarSingleLock());
+		//myRadar = new RadarStartSearch(new RadarSingleLock(new RadarNone()));
+		myRadar = new RadarStartSearch(new RadarNone());
+		System.out.format("Start ...\n");
 
-		myFireDetector = new DingoFireDetection();
-		myInactivity = new DingoInactivity();
-		myEnergyDrop = new DingoEnergyDrop();
-		myWallDistance = new WallHeadingDistance();
-
-		myBulletPaint = new PaintEnemyBulletWaves();
-		myPaintWallDistance = new PaintWallHeadingDistance(myWallDistance);
-		myPaintMaxEscape = new PaintMaxEscapeAngle();
-		myPaintEscapePath = new PaintEscapePath();
+		if (myTargetHandler == null)
+		{
+			myTargetHandler = new SimpleTargetHandler();
+		}
 	}
 
 	@Override
 	public void onStatus(StatusEvent e)
 	{
+
 		myRadar.onStatus(e);
-
-		myEnergyDrop.onStatus(e);
-		myFireDetector.onStatus(e);
-
-		myBulletPaint.onStatus(e);
-		myPaintMaxEscape.onStatus(e);
-		myPaintEscapePath.onStatus(e);
+		myTargetHandler.onStatus(e);
 	}
 
 	@Override
 	public void run()
 	{
-		myFireDetector.onInit(this);
-		myEnergyDrop.onInit(this);
-		myWallDistance.onInit(this, BORDER);
-
-		myPaintMaxEscape.onInit(this, BORDER);
-		myPaintEscapePath.onInit(this, BORDER);
-
-		setAllColors(Color.ORANGE);
-
-		System.out.format("Round: %d Rounds: %d \n", getRoundNum(), getNumRounds());
-
-		eEnergy = getEnergy();
-		eDistance = 1000;
-
+		setAllColors(Color.RED);
+		TimeProfile.initRound();
+		myTargetHandler.onInit(this);
 		myRadar.onInit(this);
 
 		while (true)
 		{
-
-			myRadar.onRun();
-
-			myEnergyDrop.onRun();
-			if (myEnergyDrop.isInactive()) myFireDetector.onInactivity();
-			myFireDetector.onRun();
-			boolean eHasFired = myFireDetector.hasFired();
-			if (eHasFired)
-			{
-				// broken after refactor
-				//myBulletPaint.onScannedRobot(,myFireDetector.getFiredBulletPower()); // takes the position of the turn before
-			}
-
-			//onMove0();
-			onMove1();
-
-			double bPower = Math.min(2.99, Math.max(0.1, Math.min(eEnergy / 4.0, 350 / eDistance)));
-			//bPower = 3.0;
-			Bullet b = null;
-			if (iFire)
-			{
-				wallHitIndex = -1;
-				b = setFireBullet(bPower);
-			}
-			if (b != null)
-			{
-				myEnergyDrop.onFire(b.getPower());
-
-				System.out.format("b0=%3.15f b1=%3.15f \n", bPower, b.getPower());
-			}
+			setTurnRadarRightRadians(myRadarTurnAmount);
+			setTurnGunRightRadians(myGunTurnAmount);
+			setTurnRightRadians(myBodyTurnAmount);
+			TimeProfile.setRoundProperties(this);
 			execute();
 		}
 	}
 
 	@Override
 	public void onHitByBullet(HitByBulletEvent e)
-	{
-		myEnergyDrop.onHitByBullet(e);
-		myFireDetector.onHitByBullet(e);
-	}
+	{}
 
 	@Override
 	public void onBulletHit(BulletHitEvent e)
-	{
-		myEnergyDrop.onBulletHit(e);
-		myFireDetector.onBulletHit(e);
-	}
+	{}
 
 	@Override
 	public void onHitRobot(HitRobotEvent e)
 	{
 		myRadar.onHitRobot(e);
-		myEnergyDrop.onHitRobot(e);
-		myFireDetector.onHitRobot(e);
 	}
 
 	@Override
 	public void onHitWall(HitWallEvent e)
-	{
-		myEnergyDrop.onHitWall(e);
-		wallHitIndex = 0;
-	}
+	{}
 
 	@Override
 	public void onScannedRobot(ScannedRobotEvent e)
 	{
-
-		double bPower = Math.min(2.99, Math.max(0.1, Math.min(e.getEnergy() / 4.0, 350 / e.getDistance())));
-
-		myPaintMaxEscape.setBulletSpeed(bPower);
-		myPaintMaxEscape.onScannedRobot(e);
-		myPaintEscapePath.setBulletSpeed(bPower);
-		myPaintEscapePath.onScannedRobot(e);
-
-		myFireDetector.onScannedRobot(e);
 		myRadar.onScannedRobot(e);
-		eAbsBearing = getHeadingRadians() + e.getBearingRadians();
-		eHeading = e.getHeadingRadians();
-		xg = getX() + Math.sin(eAbsBearing) * e.getDistance();
-		yg = getY() + Math.cos(eAbsBearing) * e.getDistance();
-
-		eBearing = e.getBearingRadians();
-		eEnergy = e.getEnergy();
-		eDistance = e.getDistance();
-
-		iFire = Utils.isNear(getGunTurnRemainingRadians(), 0.0);
-		setTurnGunRightRadians(Utils.normalRelativeAngle(eAbsBearing - getGunHeadingRadians()));
-	}
-
-	private void onMove0()
-	{
-		setTurnRightRadians(Math.sin(eBearing));
-		setAhead(1000);
-	}
-
-	private void onMove1()
-	{
-		if (Utils.isNear(getDistanceRemaining(), 0.0))
-		{
-			double heading = getHeadingRadians();
-			myWallDistance.setStartPoint(getX(), getY());
-			myWallDistance.setHeading(heading);
-			double dForward = myWallDistance.getForwardDistance();
-			double dBackward = myWallDistance.getBackwardDistance();
-
-			double dir = 1;
-			double dDist = dForward;
-			if (dForward > dBackward)
-			{
-				heading += Math.PI;
-				dir = -dir;
-				dDist = dBackward;
-			}
-			double s0 = Math.signum(Utils.normalRelativeAngle(heading));
-
-			double hDiff = 0;
-
-			setMaxVelocity(8.0);
-			if (Utils.isNear(getX(), BORDER) || Utils.isNear(getX(), getBattleFieldWidth() - BORDER))
-			{
-				// edge
-				hDiff = Math.sin(heading) * s0;
-				if (Utils.isNear(0.0, Math.toDegrees(hDiff) % 90))
-				{
-					double max = Math.max(dForward, dBackward);
-					dir = 1;
-					if (max == dBackward) dir = -1;
-					setAhead(dir * dDist * Math.random());
-				}
-			}
-			else if (Utils.isNear(getY(), BORDER) || Utils.isNear(getY(), getBattleFieldHeight() - BORDER))
-			{
-				hDiff = Math.cos(heading) * s0;
-				if (Utils.isNear(0.0, Math.toDegrees(hDiff) % 90))
-				{
-					double max = Math.max(dForward, dBackward);
-					dir = 1;
-					if (max == dBackward) dir = -1;
-					setAhead(dir * dDist * Math.random());
-				}
-			}
-			else
-			{
-				setAhead(dDist * dir);
-			}
-
-			setTurnRightRadians(hDiff);
-
-//			System.out.format("F=%3.5f B=%3.5f D=%2.2f hDiff=%3.10f\n", dForward, dBackward, dir,
-//					Math.toDegrees(hDiff) % 90);
-		}
-		setMaxVelocity(Math.random() * 4.0 + 5.0);
+		myTargetHandler.onScannedRobot(e);
 	}
 
 	@Override
 	public void onPaint(Graphics2D g)
 	{
-//		PaintRobotPath.onPaint(g, "", getTime(), getX(), getY(), Color.YELLOW);
+		myTargetHandler.onPaint(g);
+	}
 
-//		myPaint.onPaint(g);
-//		myBasics.onPaint(g);
-//		myBasics.edgeDiagonals(g, getX(), getY(), getHeadingRadians(), Color.GREEN);
-//		myBasics.edgeDiagonals(g, xg, yg, eHeading, Color.BLUE);
-//
+	// Interface functions for all devices t register there values and states
 
-		myPaintWallDistance.onPaint(g);
-		myPaintMaxEscape.onPaint(g);
-		myPaintEscapePath.onPaint(g);
-		myBulletPaint.onPaint(g);
+	@Override
+	public void setRadarTurnAmount(double turnAmount, String turnSource)
+	{
+		myRadarTurnAmount = turnAmount;
+	}
+
+	@Override
+	public void setGunTurnAmount(double turnAmount, String turnSource)
+	{
+		myGunTurnAmount = turnAmount;
+	}
+
+	@Override
+	public void setBodyTurnAmount(double turnAmount, String turnSource)
+	{
+		myBodyTurnAmount = turnAmount;
+	}
+
+	@Override
+	public void setRadar(ARadar newRadar)
+	{
+		if (newRadar != myRadar)
+		{
+			newRadar.onInit(this);
+		}
+
+		myRadar = newRadar;
+	}
+
+	@Override
+	public ATargetHandler getTargetHandler()
+	{
+		return myTargetHandler;
+	}
+
+	@Override
+	public void setTargetHandler(ATargetHandler tHandler)
+	{
+		myTargetHandler = tHandler;
 	}
 }
